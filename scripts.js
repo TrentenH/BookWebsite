a1f6dc4f3ba628cc0a344b504f38c49f37c3bd7e
trentenH <thiggins5@mail.csuchico.edu>
2021-08-17 13:17:58 -0700

    Now allows for card tiles to be deleted from the page as well as the array of books.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ let Book = function(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
+    this.id = 0
 
     this.info = function() {
         let read_string = read ? "read" : "not yet read";
@@ -30,18 +31,13 @@ function addBookToLibrary() {
     newBook.author = author;
     newBook.pages = pages;
     newBook.read = read;
+    newBook.idNum = myLibrary.length;
 
     // Stores book in array of books
     myLibrary.push(newBook);
 
     // Calls function to make displayable card
     makeBookCard(newBook);
-
-    console.log(myLibrary.length);
-    // test
-    for(let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i]);
-    }
 
     // Said to help not refresh page upon submit
     return false;
@@ -58,6 +54,7 @@ function makeBookCard(Book) {
     // Creates components of bootstrap card object
     let card = document.createElement('div');
     card.className = 'card';
+    card.id = Book.idNum;
     card.style = 'width: 18rem';
     let cardBody = document.createElement('div');
     cardBody.className = 'card-body';
@@ -65,19 +62,35 @@ function makeBookCard(Book) {
     title.innerHTML = Book.title;
     title.className = 'card-title';
     let description = document.createElement('p');
-    description.innerHTML = '<strong>By:</strong> ' + Book.author + '<br/>' + '<strong>Pages:</strong> ' + Book.pages + '<br/>' + '<strong>Read:</strong> ' + readOrNot;
+    description.innerHTML = '<strong>By:</strong> ' + Book.author + '<br/>' 
+                            + '<strong>Pages:</strong> ' + Book.pages + '<br/>'
+                             + '<strong>Read:</strong> ' + readOrNot;
     description.className = 'card-text'
+    let delButton = document.createElement('button');
+    delButton.className = 'btn btn-danger';
+    delButton.id = 'delbutton' + Book.idNum;
+    delButton.innerHTML = 'delete';
+    
 
     // Links all components made prior to their respective parents 
     // and appends the card to the DOM
     cardBody.appendChild(title);
     cardBody.appendChild(description);
+    cardBody.appendChild(delButton);
     card.appendChild(cardBody);
     container.appendChild(card);
 
-}
-
-// Adds books from myLibrary to html page
-function displayBooks() {
-
+    // Binds delete function to card button click
+    // also reassigns idNums for books so as to keep myLibrary
+    // indexing
+    delButton.onclick = function(){
+        console.log(this.id);
+        let cardID = parseInt(this.parentNode.parentNode.id);
+        myLibrary.splice(cardID, 1);
+        this.parentNode.parentNode.remove();
+        console.table(myLibrary);
+        for(let i = myLibrary.length -1; i > cardID; i--) {
+            myLibrary[i].idNum--;
+        }
+    }
 }
